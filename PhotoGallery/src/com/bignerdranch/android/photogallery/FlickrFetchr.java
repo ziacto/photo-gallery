dd58ae996b1e29aa59d3012faa66225596372a01
Bryan Blanc <bryanpblanc@gmail.com>
2014-04-11 13:38:18 -0700

    Finished Chapter 28

--- PhotoGallery/src/com/bignerdranch/android/photogallery/FlickrFetchr.java
+++ PhotoGallery/src/com/bignerdranch/android/photogallery/FlickrFetchr.java
@@ -19,10 +19,14 @@ public class FlickrFetchr {
 
 	public static final String TAG = "FlickrFetchr";
 	
+	public static final String PREF_SEARCH_QUERY = "searchQuery";
+	
 	private static final String ENDPOINT = "http://api.flickr.com/services/rest/";
 	private static final String API_KEY = "0be1374412a50fdfc592029746f42d6e";
 	private static final String METHOD_GET_RECENT = "flickr.photos.getRecent";
+	private static final String METHOD_SEARCH = "flickr.photos.search";
 	private static final String PARAM_EXTRAS = "extras";
+	private static final String PARAM_TEXT = "text";
 	
 	private static final String XML_PHOTO = "photo";
 	
@@ -56,15 +60,10 @@ public class FlickrFetchr {
 		return new String(getUrlBytes(urlSpec));
 	}
 	
-	public ArrayList<GalleryItem> fetchItems() {
+	public ArrayList<GalleryItem> downloadGalleryItems(String url) {
 		ArrayList<GalleryItem> items = new ArrayList<GalleryItem>();
 		
 		try{
-			String url = Uri.parse(ENDPOINT).buildUpon()
-					.appendQueryParameter("method", METHOD_GET_RECENT)
-					.appendQueryParameter("api_key", API_KEY)
-					.appendQueryParameter(PARAM_EXTRAS, EXTRA_SMALL_URL)
-					.build().toString();
 			String xmlString = getUrl(url);
 			Log.i(TAG,"Recieved xml: "+xmlString);
 			XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
@@ -78,6 +77,25 @@ public class FlickrFetchr {
 			Log.e(TAG,"Failed to parse items",xppe);
 		}
 		return items;
+	}
+	
+	public ArrayList<GalleryItem> fetchItems(){
+		String url = Uri.parse(ENDPOINT).buildUpon()
+			.appendQueryParameter("method", METHOD_GET_RECENT)
+			.appendQueryParameter("api_key", API_KEY)
+			.appendQueryParameter(PARAM_EXTRAS, EXTRA_SMALL_URL)
+			.build().toString();
+		return downloadGalleryItems(url);
+	}
+	
+	public ArrayList<GalleryItem> search(String query){
+		String url = Uri.parse(ENDPOINT).buildUpon()
+				.appendQueryParameter("method", METHOD_SEARCH)
+				.appendQueryParameter("api_key", API_KEY)
+				.appendQueryParameter(PARAM_EXTRAS, EXTRA_SMALL_URL)
+				.appendQueryParameter(PARAM_TEXT,query)
+				.build().toString();
+		return downloadGalleryItems(url);
 	}
 	
 	void parseItems(ArrayList<GalleryItem> items, XmlPullParser parser) throws XmlPullParserException, IOException {
--- PhotoGallery/src/com/bignerdranch/android/photogallery/PhotoGalleryFragment.java
+++ PhotoGallery/src/com/bignerdranch/android/photogallery/PhotoGalleryFragment.java
@@ -2,18 +2,30 @@ package com.bignerdranch.android.photogallery;
 
 import java.util.ArrayList;
 
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.app.SearchManager;
+import android.app.SearchableInfo;
+import android.content.ComponentName;
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.SearchView;
 
 public class PhotoGalleryFragment extends Fragment {
 
@@ -28,7 +40,8 @@ public class PhotoGalleryFragment extends Fragment {
 		super.onCreate(savedInstanceState);
 		
 		setRetainInstance(true);
-		new FetchItemsTask().execute();
+		setHasOptionsMenu(true);
+		updateItems();
 		
 		mThumbnailThread = new ThumbnailDownloader<ImageView>(new Handler());
 		mThumbnailThread.setListener(new ThumbnailDownloader.Listener<ImageView>(){
@@ -41,6 +54,10 @@ public class PhotoGalleryFragment extends Fragment {
 		mThumbnailThread.start();
 		mThumbnailThread.getLooper();
 		Log.i(TAG,"Background thread started");
+	}
+	
+	public void updateItems(){
+		new FetchItemsTask().execute();
 	}
 	
 	@Override 
@@ -74,7 +91,18 @@ public class PhotoGalleryFragment extends Fragment {
 	private class FetchItemsTask extends AsyncTask<Void,Void,ArrayList<GalleryItem>>{
 		@Override
 		protected ArrayList<GalleryItem> doInBackground(Void...params){
-			return new FlickrFetchr().fetchItems();
+			Activity activity = getActivity();
+			if(activity == null){
+				return new ArrayList<GalleryItem>();
+			}
+			
+			String query = PreferenceManager.getDefaultSharedPreferences(activity).getString(FlickrFetchr.PREF_SEARCH_QUERY, null);
+			
+			if(query != null){
+				return new FlickrFetchr().search(query);
+			}else {
+				return new FlickrFetchr().fetchItems();
+			}
 		}
 		
 		@Override
@@ -104,6 +132,39 @@ public class PhotoGalleryFragment extends Fragment {
 		}
 	}
 	
+	@Override
+	@TargetApi(11)
+	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater){
+		super.onCreateOptionsMenu(menu, inflater);
+		inflater.inflate(R.menu.fragment_photo_gallery, menu);
+		if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB){
+			//Pull out the search view
+			MenuItem searchItem = menu.findItem(R.id.menu_item_search);
+			SearchView searchView = (SearchView)searchItem.getActionView();
+			
+			//Get the data from our searchable.xml as a SearchableInfo
+			SearchManager searchManager = (SearchManager)getActivity().getSystemService(Context.SEARCH_SERVICE);
+			ComponentName name = getActivity().getComponentName();
+			SearchableInfo searchInfo = searchManager.getSearchableInfo(name);
+			
+			searchView.setSearchableInfo(searchInfo);
+		}
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item){
+		switch (item.getItemId()){
+		case R.id.menu_item_search:
+			getActivity().onSearchRequested();
+			return true;
+		case R.id.menu_item_clear:
+			PreferenceManager.getDefaultSharedPreferences(getActivity()).edit().putString(FlickrFetchr.PREF_SEARCH_QUERY, null).commit();
+			updateItems();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
 	@Override
 	public void onDestroyView(){
 		super.onDestroyView();
